21993407b0a6b010a816c046dabdb4dcc8711ea0
aztao <annabellatao@gmail.com>
2023-01-04 13:24:25 -0600

    added js for dropdown

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-$("#toggle").on("click", function(){
-    $("#dropDown").slideToggle();
-  });
-  
+$("#closedToggle").on("click", function(){
+  $("#dropDown").slideToggle();
+});
+
+$("#closedToggle2").on("click", function(){
+  $("#dropDown2").slideToggle();
+});
+
+$("#closedToggle3").on("click", function(){
+  $("#dropDown3").slideToggle();
+});
+
